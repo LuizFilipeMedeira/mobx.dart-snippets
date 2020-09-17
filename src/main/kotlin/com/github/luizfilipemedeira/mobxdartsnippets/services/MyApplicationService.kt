26778f5e31f1a7c93655d12f37425c4748f0ba72
GitHub Action <action@github.com>
2020-09-17 23:10:48 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/luizfilipemedeira/mobxdartsnippets/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.luizfilipemedeira.mobxdartsnippets.services
+
+import com.github.luizfilipemedeira.mobxdartsnippets.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/luizfilipemedeira/mobxdartsnippets/services/MyProjectService.kt
+++ src/main/kotlin/com/github/luizfilipemedeira/mobxdartsnippets/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.luizfilipemedeira.mobxdartsnippets.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.luizfilipemedeira.mobxdartsnippets.MyBundle
 
 class MyProjectService(project: Project) {
 
